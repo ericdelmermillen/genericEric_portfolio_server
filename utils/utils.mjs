4f4e8e51d1c48792ea0cf32c91d7687ca4e6757d
Eric Delmer Millen <ericdelmermillen@Erics-MBP---3.local>
2025-01-26 15:31:39 -0500

    checked and adjusted functions in utils.mjs

--- utils/utils.mjs
+++ utils/utils.mjs
@@ -1,10 +1,10 @@
-import jwt from'jsonwebtoken';
+import jwt from 'jsonwebtoken';
 import dotenv from "dotenv";
+
 dotenv.config();
 
 const JWT_TOKEN_EXPIRATION_INTERVAL = process.env.JWT_TOKEN_EXPIRATION_INTERVAL;
-const JWT_REFRESH_TOKEN_EXPIRATION_INTERVAL = process.env.
-JWT_REFRESH_TOKEN_EXPIRATION_INTERVAL;
+const JWT_REFRESH_TOKEN_EXPIRATION_INTERVAL = process.env.JWT_REFRESH_TOKEN_EXPIRATION_INTERVAL;
 const JWT_SECRET = process.env.JWT_SECRET;
 const JWT_REFRESH_SECRET = process.env.JWT_REFRESH_SECRET;
 
@@ -12,7 +12,7 @@ const JWT_REFRESH_SECRET = process.env.JWT_REFRESH_SECRET;
 // test url validity
 const isValidURL = (str) => {
   if(str.includes("mailto:") || str.includes("@")) {
-    return false
+    return false;
   };
 
   const pattern = new RegExp('^(https?:\\/\\/)?' +
@@ -50,14 +50,10 @@ const isDateValid = (project_date) => {
 
 
 // generate jwt token
-const getToken = (userID) => {
-  return jwt.sign({ userID }, JWT_SECRET, { expiresIn: JWT_TOKEN_EXPIRATION_INTERVAL });
-};
+const getToken = (userID) => jwt.sign({ userID }, JWT_SECRET, { expiresIn: JWT_TOKEN_EXPIRATION_INTERVAL });
 
 // generate jwt refresh token
-const getRefreshToken = (userID) => {  
-  return jwt.sign({ userID }, JWT_REFRESH_SECRET, { expiresIn: JWT_REFRESH_TOKEN_EXPIRATION_INTERVAL });
-};
+const getRefreshToken = (userID) => jwt.sign({ userID }, JWT_REFRESH_SECRET, { expiresIn: JWT_REFRESH_TOKEN_EXPIRATION_INTERVAL });
 
 // Verify token or refreshToken
 const verifyToken = (token, tokenType) => {
@@ -102,30 +98,23 @@ const decodeJWT = (token) => {
     const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
     const jsonPayload = Buffer.from(base64, 'base64').toString('utf-8');
     return JSON.parse(jsonPayload);
-  } catch (error) {
+  } catch(error) {
     console.error('Error decoding token:', error);
     return null;
   };
 };
 
 // date format options: returns config obj for dates
-const dateFormatOptions = () => (
-  { year: 'numeric', month: '2-digit', day: '2-digit' }
-);
+const dateFormatOptions = () => ({ year: 'numeric', month: '2-digit', day: '2-digit' });
 
-
+// for datew format 26-12-2024
 const getFormattedDate = (date) => {
-  // console.log(date); // 26-12-2024
-  const [day, month, year] = date.split('-');
+  const [ day, month, year ] = date.split('-');
 
-  // console.log(`${year}-${month.padStart(2, '0')}-${day.padStart(2, '0')}`); // 2024-12-26
   return `${year}-${month.padStart(2, '0')}-${day.padStart(2, '0')}`;
 };
 
-const isValidAWSObj = (name) => {
-  const awsObjPattern = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}\.jpeg$/;
-  return awsObjPattern.test(name);
-};
+const isValidAWSObj = (name) => /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}\.jpeg$/.test(name);
 
 export {
   isValidURL,
