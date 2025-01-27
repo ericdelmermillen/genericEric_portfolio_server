71f31237a2666ed2a4b95762c746f5a221763402
Eric Delmer Millen <ericdelmermillen@Erics-MBP---3.local>
2025-01-26 23:41:02 -0500

    removed whitespace from authRoute.mjs

--- routes/authRoute.mjs
+++ routes/authRoute.mjs
@@ -15,13 +15,11 @@ import { validateRequest } from "../middleware/middleware.mjs";
 
 const authRouter = express.Router();
 
-
 // POST /api/auth/createuser
 authRouter.route('/createuser')
   .post(
     validateRequest(emailAndPasswordAreValid), 
     createUser);
-
 
 // POST /api/auth/loginuser
 authRouter.route("/loginuser")
@@ -29,13 +27,11 @@ authRouter.route("/loginuser")
     validateRequest(emailAndPasswordAreValid), 
     loginUser);
 
-
 // POST /api/auth/refreshtoken
 authRouter.route("/refreshtoken")
   .post(
     validateRequest(refreshTokenData), 
     refreshToken);
-
 
 // protected route that does not send fresh tokens
 // POST /api/auth/getsignedurl
@@ -44,12 +40,10 @@ authRouter.route("/getsignedurl")
     validateRequest(validateAuthData), 
     getSignedurl);
   
-  
 // POST /api/auth/logoutuser
 authRouter.route("/logoutuser")
   .post(
     validateRequest(validateAuthData), 
     logoutUser);
-
 
 export default authRouter;
